cc9a25ec0310cbc3238c44a012e14a76c9329d52
Nidhi Kedia <Nidhikedia2204@gmail.com>
2026-07-13 15:48:32 +0530

    Added remaining files

---
+++ EFCore8_Assignment/RetailInventory/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
+
+using var context = new AppDbContext();
+
+// Filter products with price > 1000
+Console.WriteLine("----- Filtered Products -----");
+
+var filteredProducts = await context.Products
+    .Where(p => p.Price > 1000)
+    .OrderByDescending(p => p.Price)
+    .ToListAsync();
+
+foreach (var product in filteredProducts)
+{
+    Console.WriteLine($"{product.Name} - ₹{product.Price}");
+}
+
+Console.WriteLine();
+
+// Project into DTO (Anonymous Object)
+Console.WriteLine("----- Product DTO -----");
+
+var productDTOs = await context.Products
+    .Select(p => new
+    {
+        p.Name,
+        p.Price
+    })
+    .ToListAsync();
+
+foreach (var item in productDTOs)
+{
+    Console.WriteLine($"{item.Name} - ₹{item.Price}");
+}
